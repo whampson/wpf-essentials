9832857afaa737796820400b9ff144646e7a629b
Wes Hampson <thehambone93@gmail.com>
2019-12-30 13:42:57 -0600

    Add EnumValueCollection

---
+++ WpfEssentials.Win32/Extensions/EnumValueCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Markup;
+
+namespace WpfEssentials.Win32.Extensions
+{
+    public class EnumValueCollectionExtension : MarkupExtension
+    {
+        public Type EnumType { get; set; }
+
+        public override object ProvideValue(IServiceProvider _)
+        {
+            if (EnumType != null)
+            {
+                return CreateEnumValueList(EnumType);
+            }
+
+            return default;
+        }
+
+        private List<object> CreateEnumValueList(Type enumType)
+        {
+            return Enum.GetNames(enumType)
+                .Select(name => Enum.Parse(enumType, name))
+                .ToList();
+        }
+    }
+}

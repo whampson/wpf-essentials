164946c4d64be70a4e722051ac5d6b49dbdb27bf
Wes Hampson <thehambone93@gmail.com>
2020-02-09 19:35:57 -0800

    Add more tests!

--- WpfEssentials.Tests/FullyObservableCollectionTests.cs
+++ WpfEssentials.Tests/FullyObservableCollectionTests.cs
@@ -5,24 +5,13 @@ namespace WpfEssentials.Tests
 {
     public class FullyObservableCollectionTests
     {
-        public class TestObject : ObservableObject
-        {
-            private string m_description;
-
-            public string Description
-            {
-                get { return m_description; }
-                set { m_description = value; OnPropertyChanged(); }
-            }
-        }
-
         [Fact]
-        public void ItemPropertyChangedRaised()
+        public void ModifyItem()
         {
-            List<ItemPropertyChangedEventArgs> raisedEventArgs = new List<ItemPropertyChangedEventArgs>();
+            List<ItemStateChangedEventArgs> raisedEventArgs = new List<ItemStateChangedEventArgs>();
             FullyObservableCollection<TestObject> c = new FullyObservableCollection<TestObject>();
 
-            c.ItemPropertyChanged += delegate (object sender, ItemPropertyChangedEventArgs e)
+            c.ItemStateChanged += delegate (object sender, ItemStateChangedEventArgs e)
             {
                 raisedEventArgs.Add(e);
             };
@@ -36,11 +25,52 @@ namespace WpfEssentials.Tests
             o0.Description = "Test0";
             c[1].Description = "Test1";
 
+            Assert.Empty(raisedEventArgs);
+        }
+
+        [Fact]
+        public void ModifyItemObservable()
+        {
+            List<ItemStateChangedEventArgs> raisedEventArgs = new List<ItemStateChangedEventArgs>();
+            FullyObservableCollection<TestObject2> c = new FullyObservableCollection<TestObject2>();
+
+            c.ItemStateChanged += delegate (object sender, ItemStateChangedEventArgs e)
+            {
+                raisedEventArgs.Add(e);
+            };
+
+            TestObject2 o0 = new TestObject2();
+            TestObject2 o1 = new TestObject2();
+
+            c.Add(o0);
+            c.Add(o1);
+
+            o0.Description = "Test0";
+            c[1].Description = "Test1";
+
             Assert.Equal(2, raisedEventArgs.Count);
             Assert.Equal(0, raisedEventArgs[0].ItemIndex);
             Assert.Equal(1, raisedEventArgs[1].ItemIndex);
-            Assert.Equal(nameof(TestObject.Description), raisedEventArgs[0].PropertyName);
-            Assert.Equal(nameof(TestObject.Description), raisedEventArgs[1].PropertyName);
+            Assert.Equal(nameof(TestObject2.Description), raisedEventArgs[0].PropertyName);
+            Assert.Equal(nameof(TestObject2.Description), raisedEventArgs[1].PropertyName);
         }
+
+        #region Test Objects
+        public class TestObject
+        {
+            public string Description { get; set; }
+        }
+
+        public class TestObject2 : ObservableObject
+        {
+            private string m_description;
+
+            public string Description
+            {
+                get { return m_description; }
+                set { m_description = value; OnPropertyChanged(); }
+            }
+        }
+        #endregion
     }
 }
---
+++ WpfEssentials.Tests/TypeExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WpfEssentials.Extensions;
+using Xunit;
+
+namespace WpfEssentials.Tests
+{
+    public class TypeExtensionsTests
+    {
+        [Theory]
+        [InlineData(typeof(int), false)]
+        [InlineData(typeof(TestObject), false)]
+        [InlineData(typeof(TestObject2), true)]
+        public void IsObservable(Type t, bool expectedResult)
+        {
+            Assert.Equal(expectedResult, t.IsObservable());
+        }
+
+        private class TestObject { }
+        private class TestObject2 : ObservableObject { }
+    }
+}

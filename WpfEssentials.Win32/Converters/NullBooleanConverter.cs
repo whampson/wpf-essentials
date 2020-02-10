02a9f704b96097ce489c9c1b95bf66558d400e59
Wes Hampson <thehambone93@gmail.com>
2020-02-09 17:44:00 -0800

    Add NullBooleanConverter

---
+++ WpfEssentials.Win32/Converters/NullBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfEssentials.Win32.Converters
+{
+    /// <summary>
+    /// Gets a true/false value indicating whether a value is null.
+    /// </summary>
+    public class NullBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported for this converter.");
+        }
+    }
+}
